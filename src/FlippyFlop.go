f5978a442cafb6b3e5d17d60462b7622e75836b9
Shakeel Mohamed <smohamed@splunk.com>
2018-01-16 19:22:41 -0800

    Setup main project file + gitignore

---
+++ src/FlippyFlop.go
@@ -0,0 +1,13 @@
+package src
+
+type FlippyFlop struct {
+	// TODO: implement
+}
+
+func (ff FlippyFlop) Flip() {
+	// TODO: implement
+}
+
+func (ff FlippyFlop) Flop() int {
+	return -1 // TODO: implement
+}
