3f2d1baf20f8ee603972b930f4be9c5082ec9e42
Shakeel Mohamed <smohamed@splunk.com>
2018-03-05 11:13:22 -0800

    check in progress

--- src/FlippyFlop.go
+++ src/FlippyFlop.go
@@ -1,13 +1,53 @@
-package src
+package main
+
+import (
+	"math/rand"
+	"fmt"
+)
 
 type FlippyFlop struct {
-	// TODO: implement
+	data[] int
 }
 
-func (ff FlippyFlop) Flip() {
-	// TODO: implement
+func (ff *FlippyFlop) Count() int {
+	return len(ff.data)
 }
 
-func (ff FlippyFlop) Flop() int {
-	return -1 // TODO: implement
+func (ff *FlippyFlop) Flip(i int) bool {
+	success := rand.Intn(2) == 1
+	if success {
+		if ff.Count() == cap(ff.data) {
+			int[] growth = make(int[], 10)
+			ff.data = append(ff.data, growth)
+		}
+		position := rand.Intn(ff.Count())
+		ff.data[position] = i
+	}
+	return success
+}
+
+func (ff *FlippyFlop) Flop() (int, bool) {
+
+	if ff.Count() > 0 {
+		shouldRemove := rand.Intn(2) == 1
+		position := rand.Intn(ff.Count())
+		ret := ff.data[position]
+
+		if shouldRemove {
+			ff.data = append(ff.data[:position], ff.data[position+1:]...)
+		}
+
+		return ret, shouldRemove
+	} else {
+		return -1, false
+	}
+}
+
+func main() {
+	ff := FlippyFlop{}
+	ff.Flip(5)
+	fmt.Printf("Count: %v\n", ff.Count())
+	ff.Flop()
+	ff.Flop()
+	fmt.Printf("Count: %v\n", ff.Count())
 }
